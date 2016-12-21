045f355bd6736826802aeb5a2e9048fcd683db49
Salvatore <pigrecoinfinito@gmail.com>
2016-12-21 15:21:18 +0100

    Update pti_monumenti.sql

--- pti_monumenti.sql
+++ pti_monumenti.sql
@@ -3,7 +3,7 @@ CREATE TABLE "pti_monumenti"
 "pk_uid" integer PRIMARY KEY autoincrement NOT NULL,
 "data_rilievo" date,
 "nome_monumento" text,
-"link_foto" text
+"link_foto" text,
 "note" text
 )
 
