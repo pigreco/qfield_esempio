57468ebb86f6143eb10daa5f2a98ba830f7bb7ea
Salvatore <pigrecoinfinito@gmail.com>
2016-12-21 13:30:29 +0100

    definizione tabella punti monumenti

---
+++ pti_monumenti.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "pti_monumenti" 
+(
+"pk_uid" integer PRIMARY KEY autoincrement NOT NULL,
+"data_rilievo" date,
+"nome_monumento" text,
+"link_foto" text
+)
+
+-- aggiungo colonna geometrica
+
+SELECT AddGeometryColumn ('pti_monumenti','geom',3004,'POINT','XY');
